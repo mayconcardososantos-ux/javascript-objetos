e79b4bc0425c961913fb4533a5044a2a3dcdf589
mayconcardososantos-ux <maycon.cardoso.santos@escola.pr.gov.br>
2026-08-18 15:25:32 +0000

    objetos

---
+++ objeto.js
@@ -0,0 +1,10 @@
+const listaCPF = [1111111, 22222, 3333];
+
+const informacoesPessoal = ["nome", "jose", "idade", 23, "CPF", "111122223333"];
+
+console.log(informacoesPessoal[1]);
+
+const objtePessoal = {
+idade: 32,
+nome: "jose",
+};
