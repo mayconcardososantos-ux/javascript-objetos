0c342458067e9c8c6dec9be237c4145601f1f7ff
mayconcardososantos-ux <maycon.cardoso.santos@escola.pr.gov.br>
2026-08-25 15:21:25 +0000

    tt

---
+++ acesso_pontos.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome "Andre",
+    idade: 32,
+    cpf:"1122233345",
+    email: "andre@dominio.com",
+};
+
+console.log(
+    `O nome do cliente é ${cliente.nome} e ess pessoa tem ${cliente.idade} anos.`
+);
+
+    console.log(`Os 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
+
+    const chaves = ["nome", "idade", "CPF", "email"];
+
+    chaves.forEach((chave) => {
+        console.log (`a chave ${chave} tem valor ${cliente[chave]}`);
+ });
+    
